4727c37c3fa24cb780ee168afd6f51d7133916de
jon890 <sks070400>
2020-04-08 23:12:47 +0900

    - Posts API 만들어보기 및 테스트코드 작성

---
+++ src/main/java/com/bifos/foslog/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.bifos.foslog.web;
+
+import com.bifos.foslog.service.posts.PostsService;
+import com.bifos.foslog.web.dto.PostsResponseDto;
+import com.bifos.foslog.web.dto.PostsSaveRequestDto;
+import com.bifos.foslog.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
