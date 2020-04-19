0025b1a4c61b7dddbc2f534a3705aa676d42b722
jon890 <sks070400>
2020-04-19 14:32:50 +0900

    API 수정, 삭제 기능 및 화면 만들기

--- src/main/java/com/bifos/foslog/service/posts/PostsService.java
+++ src/main/java/com/bifos/foslog/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.bifos.foslog.service.posts;
 
 import com.bifos.foslog.domain.posts.Posts;
 import com.bifos.foslog.domain.posts.PostsRepository;
+import com.bifos.foslog.web.dto.PostsListResponseDto;
 import com.bifos.foslog.web.dto.PostsResponseDto;
 import com.bifos.foslog.web.dto.PostsSaveRequestDto;
 import com.bifos.foslog.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,10 +34,24 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
